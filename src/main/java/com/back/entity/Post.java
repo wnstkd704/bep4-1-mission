7679d72fd9cf9d48b856308c8e27e9647ce8d578
junsang <wnstkd704@gmail.com>
2026-09-17 15:03:29 +0900

    [윤준상/완료] 0003

--- src/main/java/com/back/entity/Post.java
+++ src/main/java/com/back/entity/Post.java
@@ -2,11 +2,14 @@ package com.back.entity;
 
 
 import com.back.jpa.entity.BaseIdAndTime;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static jakarta.persistence.CascadeType.PERSIST;
+import static jakarta.persistence.CascadeType.REMOVE;
 import static jakarta.persistence.FetchType.LAZY;
 
 @Entity
@@ -17,10 +20,24 @@ public class Post extends BaseIdAndTime {
     private String title;
     @Column(columnDefinition = "LONGTEXT")
     private String content;
+    @OneToMany(mappedBy = "post", cascade = {PERSIST, REMOVE}, orphanRemoval = true)
+    private List<PostComment> comments = new ArrayList<>();
 
     public Post(Member author, String title, String content) {
         this.author = author;
         this.title = title;
         this.content = content;
+    }
+
+    public PostComment addComment(Member author, String content) {
+        PostComment postComment = new PostComment(this, author, content);
+
+        comments.add(postComment);
+
+        return postComment;
+    }
+
+    public boolean hasComments() {
+        return !comments.isEmpty();
     }
 }
--- src/main/java/com/back/service/PostService.java
+++ src/main/java/com/back/service/PostService.java
@@ -6,6 +6,8 @@ import com.back.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class PostService {
@@ -19,5 +21,9 @@ public class PostService {
         Post post = new Post(author, title, content);
 
         return postRepository.save(post);
+    }
+
+    public Optional<Post> findById(int id) {
+        return postRepository.findById(id);
     }
 }
