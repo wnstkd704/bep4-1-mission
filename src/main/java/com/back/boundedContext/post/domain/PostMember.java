0a877889274b3c627ca0ac21996dc8795be1ef21
junsang <wnstkd704@gmail.com>
2026-09-18 14:05:25 +0900

    [윤준상/완료] 0017

--- src/main/java/com/back/boundedContext/post/domain/PostMember.java
+++ src/main/java/com/back/boundedContext/post/domain/PostMember.java
@@ -1,5 +1,6 @@
 package com.back.boundedContext.post.domain;
 
+import com.back.boundedContext.member.domain.ReplicaMember;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Getter;
