e251839760991b43f48146a4d1a5347a6114b64a
junsang <wnstkd704@gmail.com>
2026-09-18 12:16:36 +0900

    [윤준상/완료] 0013

--- src/main/java/com/back/boundedContext/member/app/MemberFacade.java
+++ src/main/java/com/back/boundedContext/member/app/MemberFacade.java
@@ -1,23 +1,21 @@
 package com.back.boundedContext.member.app;
 
 import com.back.boundedContext.member.domain.Member;
+import com.back.boundedContext.member.domain.MemberPolicy;
 import com.back.boundedContext.member.out.MemberRepository;
-import com.back.global.exception.DomainException;
 import com.back.global.rsData.RsData;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class MemberFacade {
     private final MemberRepository memberRepository;
     private final MemberJoinUseCase memberJoinUseCase;
-
-    public MemberFacade(MemberRepository memberRepository, MemberJoinUseCase memberJoinUseCase) {
-        this.memberRepository = memberRepository;
-        this.memberJoinUseCase = memberJoinUseCase;
-    }
+    private final MemberPolicy memberPolicy;
 
     @Transactional(readOnly = true)
     public long count() {
@@ -26,10 +24,6 @@ public class MemberFacade {
 
     @Transactional
     public RsData<Member> join(String username, String password, String nickname) {
-        findByUsername(username).ifPresent(m -> {
-            throw new DomainException("409-1", "이미 존재하는 username 입니다.");
-        });
-
         return memberJoinUseCase.join(username, password, nickname);
     }
 
@@ -38,9 +32,13 @@ public class MemberFacade {
         return memberRepository.findByUsername(username);
     }
 
-
     @Transactional(readOnly = true)
     public Optional<Member> findById(int id) {
         return memberRepository.findById(id);
+    }
+
+    public String getRandomSecureTip() {
+        return "비밀번호의 유효기간은 %d일 입니다."
+                .formatted(memberPolicy.getNeedToChangePasswordDays());
     }
 }
