183a6098c2a872ac8908a23a4d64bcc26452d6df
junsang <wnstkd704@gmail.com>
2026-09-21 13:40:13 +0900

    [윤준상/완료] 0028

--- src/main/java/com/back/boundedContext/post/app/PostFacade.java
+++ src/main/java/com/back/boundedContext/post/app/PostFacade.java
@@ -15,13 +15,13 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class PostFacade {
-    private final PostRepository postRepository;
-    private final PostMemberRepository postMemberRepository;
+    private final PostSupport postSupport;
+    private final PostSyncMemberUseCase postSyncMemberUseCase;
     private final PostWriteUseCase postWriteUseCase;
 
-    @Transactional(readOnly = true)
-    public long count() {
-        return postRepository.count();
+    @Transactional
+    public PostMember syncMember(MemberDto member) {
+        return postSyncMemberUseCase.syncMember(member);
     }
 
     @Transactional
@@ -30,27 +30,17 @@ public class PostFacade {
     }
 
     @Transactional(readOnly = true)
-    public Optional<Post> findById(int id) {
-        return postRepository.findById(id);
+    public long count() {
+        return postSupport.count();
     }
 
-    @Transactional
-    public PostMember syncMember(MemberDto member) {
-        PostMember _member = new PostMember(
-                member.getId(),
-                member.getCreateDate(),
-                member.getModifyDate(),
-                member.getUsername(),
-                "",
-                member.getNickname(),
-                member.getActivityScore()
-        );
-
-        return postMemberRepository.save(_member);
+    @Transactional(readOnly = true)
+    public Optional<Post> findById(int id) {
+        return postSupport.findById(id);
     }
 
     @Transactional(readOnly = true)
     public Optional<PostMember> findMemberByUsername(String username) {
-        return postMemberRepository.findByUsername(username);
+        return postSupport.findMemberByUsername(username);
     }
 }
