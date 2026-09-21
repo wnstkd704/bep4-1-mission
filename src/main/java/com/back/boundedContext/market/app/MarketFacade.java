8988c340e01ae9c466a1f79c07e39a1218854ba1
junsang <wnstkd704@gmail.com>
2026-09-21 16:06:20 +0900

    [윤준상/완료] 0032

--- src/main/java/com/back/boundedContext/market/app/MarketFacade.java
+++ src/main/java/com/back/boundedContext/market/app/MarketFacade.java
@@ -1,18 +1,54 @@
 package com.back.boundedContext.market.app;
 
 import com.back.boundedContext.market.domain.MarketMember;
+import com.back.boundedContext.market.domain.Product;
 import com.back.boundedContext.shared.member.dto.MemberDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class MarketFacade {
+    private final MarketSupport marketSupport;
+    private final MarketCreateProductUseCase marketCreateProductUseCase;
     private final MarketSyncMemberUseCase marketSyncMemberUseCase;
 
     @Transactional
     public MarketMember syncMember(MemberDto member) {
         return marketSyncMemberUseCase.syncMember(member);
+    }
+
+    @Transactional(readOnly = true)
+    public long productsCount() {
+        return marketSupport.countProducts();
+    }
+
+    @Transactional
+    public Product createProduct(
+            MarketMember seller,
+            String sourceTypeCode,
+            int sourceId,
+            String name,
+            String description,
+            long price,
+            long salePrice
+    ) {
+        return marketCreateProductUseCase.createProduct(
+                seller,
+                sourceTypeCode,
+                sourceId,
+                name,
+                description,
+                price,
+                salePrice
+        );
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<MarketMember> findMemberByUsername(String username) {
+        return marketSupport.findMemberByUsername(username);
     }
 }
