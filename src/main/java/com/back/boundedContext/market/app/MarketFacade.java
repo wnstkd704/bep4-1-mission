c5e52cd697722f351c54e9b86d1dfb4b81b675e2
junsang <wnstkd704@gmail.com>
2026-09-21 22:33:36 +0900

    [윤준상/완료] 0034

--- src/main/java/com/back/boundedContext/market/app/MarketFacade.java
+++ src/main/java/com/back/boundedContext/market/app/MarketFacade.java
@@ -2,6 +2,7 @@ package com.back.boundedContext.market.app;
 
 import com.back.boundedContext.market.domain.Cart;
 import com.back.boundedContext.market.domain.MarketMember;
+import com.back.boundedContext.market.domain.Order;
 import com.back.boundedContext.market.domain.Product;
 import com.back.boundedContext.shared.market.dto.MarketMemberDto;
 import com.back.boundedContext.shared.member.dto.MemberDto;
@@ -19,6 +20,7 @@ public class MarketFacade {
     private final MarketCreateProductUseCase marketCreateProductUseCase;
     private final MarketSyncMemberUseCase marketSyncMemberUseCase;
     private final MarketCreateCartUseCase marketCreateCartUseCase;
+    private final MarketCreateOrderUseCase marketCreateOrderUseCase;
 
     @Transactional
     public MarketMember syncMember(MemberDto member) {
@@ -69,5 +71,15 @@ public class MarketFacade {
     @Transactional(readOnly = true)
     public Optional<Product> findProductById(int id) {
         return marketSupport.findProductById(id);
+    }
+
+    @Transactional(readOnly = true)
+    public long ordersCount() {
+        return marketSupport.countOrders();
+    }
+
+    @Transactional
+    public RsData<Order> createOrder(Cart cart) {
+        return marketCreateOrderUseCase.createOrder(cart);
     }
 }
--- src/main/java/com/back/boundedContext/market/domain/Cart.java
+++ src/main/java/com/back/boundedContext/market/domain/Cart.java
@@ -40,4 +40,8 @@ public class Cart extends BaseManualIdAndTime {
         this.getItems().add(cartItem);
         this.itemsCount++;
     }
+
+    public void clearItems() {
+        this.getItems().clear();
+    }
 }
