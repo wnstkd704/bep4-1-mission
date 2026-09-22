3a9bb97e81adb7b60ac6aca99eaa5f1a870f5696
junsang <wnstkd704@gmail.com>
2026-09-22 12:10:15 +0900

    [윤준상/완료] 0037

--- src/main/java/com/back/boundedContext/market/domain/MarketPolicy.java
+++ src/main/java/com/back/boundedContext/market/domain/MarketPolicy.java
@@ -1,5 +1,12 @@
 package com.back.boundedContext.market.domain;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class MarketPolicy {
-    public static final double PRODUCT_PAYOUT_RATE = 90;
+    public static double PRODUCT_PAYOUT_RATE;
+
+    @Value("${custom.market.product.payoutRate}")
+    public void setProductPayoutRate(double rate) {
+        PRODUCT_PAYOUT_RATE = rate;
+    }
 }
--- src/main/java/com/back/boundedContext/member/domain/MemberPolicy.java
+++ src/main/java/com/back/boundedContext/member/domain/MemberPolicy.java
@@ -1,5 +1,6 @@
 package com.back.boundedContext.member.domain;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
@@ -7,8 +8,12 @@ import java.time.LocalDateTime;
 
 @Service
 public class MemberPolicy {
-    private static final int PASSWORD_CHANGE_DAYS = 90;
+    private static int PASSWORD_CHANGE_DAYS;
 
+    @Value("${custom.member.password.changeDays}")
+    public void setPasswordChangeDays(int days) {
+        PASSWORD_CHANGE_DAYS = days;
+    }
     public Duration getNeedToChangePasswordPeriod() {
         return Duration.ofDays(PASSWORD_CHANGE_DAYS);
     }
