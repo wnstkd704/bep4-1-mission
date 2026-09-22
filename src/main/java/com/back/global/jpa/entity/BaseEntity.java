f4afc51a9952057f1ce7ca479043cadaf05c049f
junsang <wnstkd704@gmail.com>
2026-09-22 15:10:08 +0900

    [윤준상/완료] 0040

--- src/main/java/com/back/global/jpa/entity/BaseEntity.java
+++ src/main/java/com/back/global/jpa/entity/BaseEntity.java
@@ -1,6 +1,7 @@
 package com.back.global.jpa.entity;
 
 import com.back.global.global.GlobalConfig;
+import com.back.standard.modelType.HasModelTypeCode;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 
@@ -9,13 +10,14 @@ import java.time.LocalDateTime;
 @MappedSuperclass
 @Getter
 // 모든 엔티티들의 조상
-public abstract class BaseEntity {
+public abstract class BaseEntity implements HasModelTypeCode {
     public abstract int getId();
 
     public abstract LocalDateTime getCreateDate();
 
     public abstract LocalDateTime getModifyDate();
 
+    @Override
     public String getModelTypeCode() {
         return this.getClass().getSimpleName();
     }
