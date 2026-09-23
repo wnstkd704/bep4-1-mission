59fcddec77a766265e2c60bf4556c1737bfaa6dc
junsang <wnstkd704@gmail.com>
2026-09-23 09:46:49 +0900

    [윤준상/완료] 0045

--- src/main/java/com/back/boundedContext/payout/app/PayoutSupport.java
+++ src/main/java/com/back/boundedContext/payout/app/PayoutSupport.java
@@ -1,16 +1,20 @@
 package com.back.boundedContext.payout.app;
 
+import com.back.boundedContext.payout.domain.PayoutCandidateItem;
 import com.back.boundedContext.payout.domain.PayoutMember;
+import com.back.boundedContext.payout.out.PayoutCandidateItemRepository;
 import com.back.boundedContext.payout.out.PayoutMemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class PayoutSupport {
     private final PayoutMemberRepository payoutMemberRepository;
+    private final PayoutCandidateItemRepository payoutCandidateItemRepository;
 
     public Optional<PayoutMember> findSystemMember() {
         return payoutMemberRepository.findByUsername("system");
@@ -18,5 +22,9 @@ public class PayoutSupport {
 
     public Optional<PayoutMember> findMemberById(int id) {
         return payoutMemberRepository.findById(id);
+    }
+
+    public List<PayoutCandidateItem> findPayoutCandidateItems() {
+        return payoutCandidateItemRepository.findAll();
     }
 }
